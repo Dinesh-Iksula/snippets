fb1c0204c2a9fe4c1e53130aafb19c1d0f40da55
Dinesh <dinurait@gmail.com>
2018-07-14 23:43:11 +0530

    First snippet writetxt complete

---
+++ writetxt/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo $data = "Today is " . date("D, d - M - Y H:i:s") . " and default time zone is: " . date_default_timezone_get();
+
+$data = PHP_EOL.$data.PHP_EOL;
+
+$my_file = 'write.txt';
+$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+fwrite($handle, $data);
+fclose($handle);
